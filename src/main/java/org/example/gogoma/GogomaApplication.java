0a34938a4efe95159d7511f1316120d83aa23848
이재훈 <dlwogns1205@naver.com>
2025-01-27 13:46:46 +0900

    init project

---
+++ src/main/java/org/example/gogoma/GogomaApplication.java
@@ -0,0 +1,13 @@
+package org.example.gogoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gogoma/GogomaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gogoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
