ae60d0c6c20b37bd5b5c9412968aec2e316f8aaa
이주호 <gernica10@gmail.com>
2025-01-29 21:19:48 +0900

    feat: "마이페이지" 추가 [S12P11A808-21]

---
+++ app/src/main/java/com/example/gogoma/ui/screens/MypageScreen.kt
@@ -0,0 +1,10 @@
+package com.example.gogoma.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MypageScreen(navController: NavController) {
+    Text("마이페이지입니다")
+}
