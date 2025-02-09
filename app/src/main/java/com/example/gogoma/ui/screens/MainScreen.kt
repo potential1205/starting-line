a69122f9113b7416c782fccab5cb3f59fbb64e55
jimin baek <eosa1414@gmail.com>
2025-02-10 02:58:38 +0900

    feat: "메인 페이지 UI 정리(메인 완료)" 디스플레이 가로 길이에 따라  깨지는 현상 수정(화면 최적화)

--- app/src/main/java/com/example/gogoma/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/gogoma/ui/screens/MainScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -70,9 +71,9 @@ fun MainScreen(
                 item {
                     CircularProgressIndicator(
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxSize()
                             .padding(16.dp),
-                        color = Color.Blue
+                        color = MaterialTheme.colorScheme.primary
                     )
                 }
             } else {
