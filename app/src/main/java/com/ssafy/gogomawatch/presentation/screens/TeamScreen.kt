c51b955a2ad471497abc02aa72140f1964844b02
박민경 <hogik1027@naver.com>
2025-02-10 02:03:16 +0900

    feat: '드래그 및 포커싱 구현' 가운데 인덱스 크기 조정 추가 및 드래그 기능 추가 [S12P11A808-182]

--- app/src/main/java/com/ssafy/gogomawatch/presentation/screens/TeamScreen.kt
+++ app/src/main/java/com/ssafy/gogomawatch/presentation/screens/TeamScreen.kt
@@ -1,7 +1,11 @@
 package com.ssafy.gogomawatch.presentation.screens
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,25 +14,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.Text
+import androidx.wear.tooling.preview.devices.WearDevices
 import com.ssafy.gogomawatch.R
-import com.ssafy.gogomawatch.presentation.components.ProgressBar
 
 @Composable
 fun TeamScreen() {
     Box() {
+        // ProgressBar
         Box(
             modifier = Modifier
                 .fillMaxSize(),
@@ -42,21 +50,27 @@ fun TeamScreen() {
                 trackColor = Color.Green.copy(alpha = 0.2f)
             )
         }
+        // 내부 길 및 랭킹
         Row (
-            Modifier.fillMaxSize()
-                .padding(30.dp)
+            Modifier
+                .fillMaxSize(0.65f)
+                .align(Alignment.Center)
         ) {
+            // 길 이미지
             Box(
-                Modifier.fillMaxWidth(0.4f)
+                Modifier
+                    .fillMaxWidth(0.4f)
                     .fillMaxHeight()
                     .padding(start = 5.dp, end = 5.dp)
             ) {
                 Box(
-                    Modifier.fillMaxSize()
+                    Modifier
+                        .fillMaxSize()
                         .background(color = Color.White)
                 )
                 Box(
-                    Modifier.fillMaxSize()
+                    Modifier
+                        .fillMaxSize()
                         .align(Alignment.Center)
                 ) {
                     Image(
@@ -82,16 +96,67 @@ fun TeamScreen() {
                 "0tem"
             )
 
+            // 등수 부분
+            // 위치 재정비
+            val lazyListState = rememberLazyListState()
+
+            val density = LocalDensity.current
+
+            val snappingLayout = remember(lazyListState, density) {
+                val snapPosition = object : SnapPosition {
+                    override fun position(
+                        layoutSize: Int,
+                        itemSize: Int,
+                        beforeContentPadding: Int,
+                        afterContentPadding: Int,
+                        itemIndex: Int,
+                        itemCount: Int
+                    ): Int {
+                        // 보이는 아이템의 index와 offset
+                        if (lazyListState.layoutInfo.visibleItemsInfo.size > 1) {
+                            val firstIndex = lazyListState.layoutInfo.visibleItemsInfo[0].index
+                            val firstOffset = lazyListState.layoutInfo.visibleItemsInfo[0].offset
+                            val secondIndex = lazyListState.layoutInfo.visibleItemsInfo[1].index
+                            val secondOffset = lazyListState.layoutInfo.visibleItemsInfo[1].offset
+                        }
+
+                        return 0 // item의 움직일 포지션값
+                    }
+                }
+                SnapLayoutInfoProvider(lazyListState, snapPosition)
+            }
+
+            val flingBehavior = rememberSnapFlingBehavior(snappingLayout)
+
+            // 스크롤 중이냐 아니냐 check. 멈추면 보이고 있는 첫번째 index로 animateScroll
+            LaunchedEffect(key1 = lazyListState.isScrollInProgress) {
+                if (!lazyListState.isScrollInProgress) {
+                    lazyListState.animateScrollToItem(lazyListState.firstVisibleItemIndex)
+                }
+            }
+
             LazyColumn (
-                Modifier
-                    .fillMaxHeight()
+                state = lazyListState,
+                modifier = Modifier
+                    .fillMaxHeight(),
+                flingBehavior = flingBehavior
             ){
-                items(itemsList) { item ->
+                items(itemsList.size) { index ->
+                    val isCenterItem = index == lazyListState.firstVisibleItemIndex + 1
+                    val scale by animateFloatAsState(
+                        targetValue = if (isCenterItem) 1.3f else 1.0f,
+                        label = "itemScale"
+                    )
                     Box(
-                        Modifier.height(50.dp)
+                        Modifier.height(40.dp)
+                            .fillMaxWidth()
                     ) {
-                        Text(text = item,
-                            fontSize = 30.sp)
+                        Text(
+                            text = itemsList[index],
+                            fontSize = (25.sp * scale),
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                        )
                     } // 리스트 아이템을 텍스트로 표시
                 }
             }
@@ -100,7 +165,7 @@ fun TeamScreen() {
 
 }
 
-@Preview
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun TeamScreenPreview() {
     TeamScreen()
