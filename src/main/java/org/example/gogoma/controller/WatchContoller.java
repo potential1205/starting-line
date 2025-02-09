11c5bb7791b132e575e82acf3761ba0ae7fdc48c
이재훈 <dlwogns1205@naver.com>
2025-02-09 14:55:17 +0900

    feat: "마라톤 시작 데이터 전송 API 기능 개발" 불필요한 메서드 제거 [S12P11A808-227]

--- src/main/java/org/example/gogoma/controller/WatchContoller.java
+++ src/main/java/org/example/gogoma/controller/WatchContoller.java
@@ -16,31 +16,6 @@ public class WatchContoller {
 
     private final WatchService watchService;
 
-    @GetMapping("/ranking")
-    public ResponseEntity<List<String>> getRanking() {
-        List<String> rankingList = Arrays.asList(
-                "",
-                "1st 김지수",
-                "2nd 김용현",
-                "3rd 박민경",
-                "4th 백지민",
-                "5th 이재훈",
-                "6th 이주호",
-                ""
-        );
-
-        System.out.println("Send a message to the watch");
-
-        return ResponseEntity.ok(rankingList);
-    }
-
-    @PostMapping("/send")
-    public ResponseEntity<String> sendString(@RequestBody String request) {
-        System.out.println("Receive a message from the watch " + request);
-
-        return ResponseEntity.ok().build();
-    }
-
     @PostMapping("/start/{userId}")
     public ResponseEntity<String> sendMarathonStartInitData(@PathVariable int userId) {
 
