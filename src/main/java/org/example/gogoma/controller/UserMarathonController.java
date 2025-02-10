fba551f0409ef46b8abb71b21ebe1f5059f84fd8
이재훈 <dlwogns1205@naver.com>
2025-02-10 14:00:06 +0900

    feat: "다가온 대회 조회 기능 개발" 사용하지 않는 함수 및 코드 제거[S12P11A808-250]

--- src/main/java/org/example/gogoma/controller/UserMarathonController.java
+++ src/main/java/org/example/gogoma/controller/UserMarathonController.java
@@ -4,13 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.gogoma.controller.response.UpcomingMarathonInfoResponse;
 import org.example.gogoma.controller.response.UserMarathonDetailResponse;
 import org.example.gogoma.controller.response.UserMarathonSearchResponse;
-import org.example.gogoma.domain.user.entity.User;
-import org.example.gogoma.domain.user.repository.UserRepository;
 import org.example.gogoma.domain.usermarathon.service.UserMarathonService;
-import org.example.gogoma.exception.ExceptionCode;
-import org.example.gogoma.exception.type.DbException;
-import org.example.gogoma.external.kakao.oauth.KakaoOauthClient;
-import org.example.gogoma.external.kakao.oauth.KakaoUserInfo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/usermarathons")
 public class UserMarathonController {
 
-    private final UserRepository userRepository;
-    private final KakaoOauthClient kakaoOauthClient;
     private final UserMarathonService userMarathonService;
 
     @GetMapping
@@ -52,6 +44,4 @@ public class UserMarathonController {
 
         return ResponseEntity.ok(upcomingMarathonInfoResponse);
     }
-
-
 }
