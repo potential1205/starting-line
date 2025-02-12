71dff810e63a2013d18245bf903ad072773aefa4
이재훈 <dlwogns1205@naver.com>
2025-02-12 18:57:41 +0900

    feat: "같은 대회에 참가한 친구 목록 조회 기능" 쿼리 수정 [S12P11A808-310]

---
+++ src/main/java/org/example/gogoma/domain/user/repository/FriendCustomRepositoryImpl.java
@@ -0,0 +1,48 @@
+package org.example.gogoma.domain.user.repository;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.example.gogoma.domain.user.entity.Friend;
+import org.springframework.stereotype.Repository;
+
+import static org.example.gogoma.domain.user.entity.QFriend.friend;
+import static org.example.gogoma.domain.usermarathon.entity.QUserMarathon.userMarathon;
+
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class FriendCustomRepositoryImpl implements FriendCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public FriendCustomRepositoryImpl(EntityManager entityManager) {
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+
+    @Override
+    public List<Friend> findAllByUserIdAndMarathonId(int userId, int marathonId) {
+            List<Integer> friendIds = queryFactory
+                    .select(friend.friendId)
+                    .from(friend)
+                    .where(friend.userId.eq(userId))
+                    .fetch();
+
+            if (friendIds.isEmpty()) {
+                return Collections.emptyList();
+            }
+
+            return queryFactory
+                    .selectFrom(friend)
+                    .where(friend.userId.in(
+                            JPAExpressions
+                                    .select(userMarathon.userId)
+                                    .from(userMarathon)
+                                    .where(userMarathon.marathonId.eq(marathonId)
+                                            .and(userMarathon.userId.in(friendIds))
+                    )))
+                    .fetch();
+    }
+}
--- src/main/java/org/example/gogoma/domain/watch/service/WatchServiceImpl.java
+++ src/main/java/org/example/gogoma/domain/watch/service/WatchServiceImpl.java
@@ -6,6 +6,7 @@ import org.example.gogoma.domain.marathon.entity.Marathon;
 import org.example.gogoma.domain.marathon.repository.MarathonRepository;
 import org.example.gogoma.domain.user.entity.Friend;
 import org.example.gogoma.domain.user.entity.User;
+import org.example.gogoma.domain.user.repository.FriendCustomRepository;
 import org.example.gogoma.domain.user.repository.FriendRepository;
 import org.example.gogoma.domain.user.repository.UserRepository;
 import org.example.gogoma.domain.user.service.UserService;
@@ -27,6 +28,7 @@ public class WatchServiceImpl implements WatchService {
     private final KakaoOauthClient kakaoOauthClient;
     private final UserRepository userRepository;
     private final FriendRepository friendRepository;
+    private final FriendCustomRepository friendCustomRepository;
     private final MarathonRepository marathonRepository;
     private final UserMarathonRepository userMarathonRepository;
 
@@ -62,7 +64,7 @@ public class WatchServiceImpl implements WatchService {
         UserMarathon userMarathon = userMarathonRepository.findByUserIdAndMarathonId(user.getId(), marathonId)
                 .orElseThrow(() -> new DbException(ExceptionCode.USER_MARATHON_NOT_FOUND));
 
-        List<Friend> friendList = friendRepository.findAllByUserId(user.getId());
+        List<Friend> friendList = friendCustomRepository.findAllByUserIdAndMarathonId(user.getId(), marathon.getId());
 
         return MarathonReadyDto.of(
                 user.getId(), user.getName(), userMarathon.getTargetPace(),
