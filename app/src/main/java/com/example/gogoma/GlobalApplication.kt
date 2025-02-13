055ef67b07a30660d5c3aca5ed2303269f69f69e
김용현 <dydgus0831@gmail.com>
2025-02-13 18:08:21 +0900

    feat: "대회 준비 페이지" GlobalApplication Class 생성 [S12PA11A808-321]

--- app/src/main/java/com/example/gogoma/GlobalApplication.kt
+++ app/src/main/java/com/example/gogoma/GlobalApplication.kt
@@ -2,9 +2,11 @@ package com.example.gogoma
 
 import android.app.Application
 import android.util.Log
+import com.example.gogoma.data.model.MarathonStartInitDataResponse
 import com.kakao.sdk.common.KakaoSdk
 
 class GlobalApplication : Application() {
+    var initData: MarathonStartInitDataResponse? = null
     override fun onCreate() {
         super.onCreate()
         // 카카오 SDK 초기화
