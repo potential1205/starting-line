3f3c274aad72b1e66a266561c213ae37cda1dc82
김용현 <dydgus0831@gmail.com>
2025-02-13 15:19:15 +0900

    feat: "대회 준비 페이지" Room DB 테스트 [S12PA11A808-311]

--- app/src/main/java/com/example/gogoma/data/roomdb/dao/MyInfoDao.kt
+++ app/src/main/java/com/example/gogoma/data/roomdb/dao/MyInfoDao.kt
@@ -8,8 +8,8 @@ import com.example.gogoma.data.roomdb.entity.MyInfo
 @Dao
 interface MyInfoDao {
     @Insert
-    suspend fun insertMyInfo(myInfo: MyInfo)
+    fun insertMyInfo(myInfo: MyInfo)
 
     @Query("SELECT * FROM MyInfo limit 1 ")
-    suspend fun getMyInfo(): MyInfo?
+    fun getMyInfo(): MyInfo?
 }
--- app/src/main/java/com/example/gogoma/data/roomdb/entity/Marathon.kt
+++ app/src/main/java/com/example/gogoma/data/roomdb/entity/Marathon.kt
@@ -2,11 +2,12 @@ package com.example.gogoma.data.roomdb.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDateTime
 
 @Entity
 data class Marathon(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val name: String,
-    val startTime: Long
+    val startTime: String
 )
