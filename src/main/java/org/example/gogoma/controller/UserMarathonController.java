65d9e6ce36c808e0d1e19337ad3880f1d5461921
김지수 <comjisu0311@naver.com>
2025-02-14 00:36:50 +0900

    refact: "겹치는 로직 수정 및 Swagger 설명 추가"  [S12P11A808-201]

--- src/main/java/org/example/gogoma/controller/UserMarathonController.java
+++ src/main/java/org/example/gogoma/controller/UserMarathonController.java
@@ -1,5 +1,6 @@
 package org.example.gogoma.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.example.gogoma.common.dto.BooleanResponse;
 import org.example.gogoma.controller.request.CreateUserMarathonRequest;
@@ -18,6 +19,7 @@ public class UserMarathonController {
     private final UserMarathonService userMarathonService;
 
     @GetMapping
+    @Operation(summary = "유저 마라톤 목록 조회", description = "해당 유저의 모든 마라톤 기록 목록을 조회합니다.")
     public ResponseEntity<UserMarathonSearchResponse> searchUserMarathonList(
             @RequestHeader("Authorization") String accessToken) {
 
@@ -28,6 +30,7 @@ public class UserMarathonController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "유저 마라톤 상세 조회", description = "해당 ID에 해당하는 유저 마라톤 상세 정보를 조회합니다.")
     public ResponseEntity<UserMarathonDetailResponse> getUserMarathonById(
             @RequestHeader("Authorization") String accessToken, @PathVariable int id) {
 
@@ -38,6 +41,7 @@ public class UserMarathonController {
     }
 
     @PatchMapping("/{marathonId}")
+    @Operation(summary = "유저 마라톤 업데이트", description = "마라톤 참가 중 유저의 목표 페이스를 수정합니다.")
     public ResponseEntity<BooleanResponse> updateUserMarathon(
             @RequestHeader("Authorization") String accessToken,
             @PathVariable int marathonId,
@@ -49,6 +53,7 @@ public class UserMarathonController {
     }
 
     @GetMapping("/{marathonId}")
+    @Operation(summary = "유저 마라톤 중복 확인", description = "해당 마라톤에 유저가 이미 등록되어 있는지 중복 여부를 확인합니다.")
     public ResponseEntity<BooleanResponse> checkDuplicateUserMarathon(
             @RequestHeader("Authorization") String accessToken,
             @PathVariable int marathonId) {
@@ -59,6 +64,7 @@ public class UserMarathonController {
     }
 
     @PostMapping
+    @Operation(summary = "유저 마라톤 등록", description = "새로운 마라톤 참여 정보를 등록합니다.")
     public ResponseEntity<BooleanResponse> createUserMarathon(
             @RequestHeader("Authorization") String accessToken,
             @RequestBody CreateUserMarathonRequest createUserMarathonRequest) {
--- src/main/java/org/example/gogoma/controller/WatchContoller.java
+++ src/main/java/org/example/gogoma/controller/WatchContoller.java
@@ -1,5 +1,6 @@
 package org.example.gogoma.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.example.gogoma.common.dto.BooleanResponse;
 import org.example.gogoma.controller.request.MarathonEndInitDataRequest;
@@ -9,9 +10,6 @@ import org.example.gogoma.domain.watch.service.WatchService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/watch")
@@ -20,6 +18,7 @@ public class WatchContoller {
     private final WatchService watchService;
 
     @GetMapping("/start/marathons/{marathonId}")
+    @Operation(summary = "마라톤 시작 데이터 전송", description = "마라톤 시작 시 필요한 데이터를 전송합니다")
     public ResponseEntity<MarathonStartInitDataResponse> sendMarathonStartInitData(
             @RequestHeader("Authorization") String accessToken, @PathVariable int marathonId) {
 
@@ -30,6 +29,7 @@ public class WatchContoller {
     }
 
     @GetMapping("/start/marathons/{marathonId}/users/{userId}")
+    @Operation(summary = "마라톤 준비 데이터 전송", description = "마라톤 준비 시 필요한 데이터를 전송합니다.")
     public ResponseEntity<MarathonReadyDto> send(
             @PathVariable int userId, @PathVariable int marathonId) {
 
@@ -40,6 +40,7 @@ public class WatchContoller {
     }
 
     @PostMapping("/end/marathons/{marathonId}")
+    @Operation(summary = "마라톤 종료 데이터 추가", description = "마라톤 종료 시 전송받은 통계 데이터를 업데이트하여 저장합니다.")
     public ResponseEntity<BooleanResponse> updateMarathonEndData(
             @RequestHeader("Authorization") String accessToken,
             @PathVariable int marathonId,
