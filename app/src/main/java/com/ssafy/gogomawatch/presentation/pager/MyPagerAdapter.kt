899b2bfa617e426c04313dcebeed5bf9fc612f91
백지민 <eosa1414@gmail.com>
2025-02-14 09:12:19 +0900

    feat: "팀 길 화면" 뷰 페이저로 이동 가능한 팀 길 화면 추가

--- app/src/main/java/com/ssafy/gogomawatch/presentation/pager/MyPagerAdapter.kt
+++ app/src/main/java/com/ssafy/gogomawatch/presentation/pager/MyPagerAdapter.kt
@@ -3,14 +3,16 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ssafy.gogomawatch.presentation.pager.PersonalFragment
 import com.ssafy.gogomawatch.presentation.pager.TeamFragment
+import com.ssafy.gogomawatch.presentation.pager.TeamRoadFragment
 
 class MyPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> PersonalFragment()
             1 -> TeamFragment()
+            1 -> TeamRoadFragment()
             else -> throw IllegalStateException("Invalid position: $position")
         }
     }
---
+++ app/src/main/java/com/ssafy/gogomawatch/presentation/screens/TeamRoadScreen.kt
@@ -0,0 +1,8 @@
+package com.ssafy.gogomawatch.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TeamRoadScreen() {
+
+}
