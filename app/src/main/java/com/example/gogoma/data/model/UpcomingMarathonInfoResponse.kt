4f4494301258702f170f36f14ce80d2c608d269b
이재훈 <dlwogns1205@naver.com>
2025-02-19 21:41:02 +0900

    fix: "RealDataBase 초기화 시점 변경" ReadlTimeData 초기화 시점 변경 / 데이터 전송 시점 변경[S12P11A808-443]

--- app/src/main/java/com/example/gogoma/data/model/UpcomingMarathonInfoResponse.kt
+++ app/src/main/java/com/example/gogoma/data/model/UpcomingMarathonInfoResponse.kt
@@ -5,5 +5,6 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UpcomingMarathonInfoResponse(
-    var marathon: Marathon
+    var marathon: Marathon,
+    var isEnd : Boolean
 ) : Parcelable
